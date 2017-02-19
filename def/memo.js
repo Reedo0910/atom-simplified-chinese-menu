536bba2437449d975ecbe828989b91adef95df91
Reedo0910 <sea.sand@live.cn>
2017-02-19 17:50:19 +0800

    2017-02-19 17:50:15

--- def/memo.js
+++ def/memo.js
@@ -3,15 +3,15 @@
  */
 var sels = {};
 atom.contextMenu.itemSets.map(function(d){
-  if (!sels[d.selector]) { sels[d.selector] = []; }
-  sels[d.selector] = sels[d.selector].concat(d.items);
+    if (!sels[d.selector]) { sels[d.selector] = []; }
+    sels[d.selector] = sels[d.selector].concat(d.items);
 });
-var data = "";
+var data = '';
 for (var sel in sels) {
-  data += '  "' + sel + '":\n';
-  sels[sel].map(function(d){
-    if (d.type === "separator") return false;
-    data += '    "'+d.command+'": "'+d.label+'"\n';
-});
+    data += '  "' + sel + '":\n';
+    sels[sel].map(function(d){
+        if (d.type === 'separator') return false;
+        data += '    "'+d.command+'": "'+d.label+'"\n';
+    });
 }
 atom.clipboard.write(data);
